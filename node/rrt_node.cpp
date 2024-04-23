d9238e96023b2e282093290c72ae63aeff6ba272
Lakshay <kumarlakshay500@gmail.com>
2024-04-23 16:16:19 +0530

    first commit

---
+++ node/rrt_node.cpp
@@ -0,0 +1,15 @@
+
+#include "rrt/rrt.h"
+
+int main(int argc, char** argv) {
+    ros::init(argc, argv, "rrt_node");
+    ros::NodeHandle nh;
+    RRT rrt(nh);
+//    ros::Rate rate(1);
+//    while(ros::ok()){
+//        ros::spinOnce();
+//        rate.sleep();
+//    }
+    ros::spin();
+    return 0;
+}
